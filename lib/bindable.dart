7d53532471e82e53a068c13fa69e866fb2babbc9
Tobias <tobiheidi@web.de>
2019-03-17 14:25:25 +0200

    added bindable map

--- lib/bindable.dart
+++ lib/bindable.dart
@@ -2,5 +2,6 @@ library bindable;
 
 export 'src/binding/bindable.dart';
 export 'src/binding/bindable_list.dart';
+export 'src/binding/bindable_map.dart';
 export 'src/binding/binding.dart';
 export 'src/binding/binding_widget.dart';
